0b2ad1ee435fd2a1cd48805f421cf3acd068f608
Anirudh Saikrishnan <anirudh.saikrishnan@gmail.com>
2025-05-02 03:37:17 +0530

    ...

--- ws_server/pubsub/notifier.go
+++ ws_server/pubsub/notifier.go
@@ -1,6 +1,9 @@
 package pubsub
 
-import "github.com/anshikag020/EvenUp/ws_server/hub"
+import (
+    "github.com/anshikag020/EvenUp/ws_server/hub"
+    "encoding/json"
+)
 
 // NotifyExpense broadcasts a new‐expense event.
 func NotifyExpense(h *hub.Hub, payload []byte) {
@@ -10,4 +13,10 @@ func NotifyExpense(h *hub.Hub, payload []byte) {
 // NotifySettle broadcasts a settle‐up event.
 func NotifySettle(h *hub.Hub, payload []byte) {
     h.Broadcast(payload)
+}
+
+func NotifyRefresh(h *hub.Hub, page string) {
+    msg := map[string]string{"type":"refresh_page","page":page}
+    b, _ := json.Marshal(msg)
+    h.Broadcast(b)
 }
